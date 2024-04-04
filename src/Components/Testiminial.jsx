30f85abb932b3baa28991885498d0cc5cf221cc5
kponky <ikponkedet@gmail.com>
2024-04-04 09:46:08 +0100

    swiper color changed

--- src/Components/Testiminial.jsx
+++ src/Components/Testiminial.jsx
@@ -18,7 +18,7 @@ const Testiminial = () => {
     spaceBetween={30}
     centeredSlides = {true}
     autoplay = {{
-      delay: 2500, 
+      delay: 3000, 
     disableOnInteraction: false
   }}
   pagination = {{
